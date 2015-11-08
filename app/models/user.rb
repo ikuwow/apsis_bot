dc5e5dd6d06f83169e759a7b60e1072ccd633555
Ikuo Degawa <degawa@ikuwow.com>
2015-11-08 13:08:52 +0900

    Add validations to user model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true, length: {maximum: 32}
+    validates :email, presence: true, length: {maximum: 128}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,24 @@ class UserTest < ActiveSupport::TestCase
         assert @user.valid?
     end
 
+    test "name should be present" do
+        @user.name = "   "
+        assert_not @user.valid?
+    end
+
+    test "email should be present" do
+        @user.email = " "
+        assert_not @user.valid?
+    end
+
+    test "name should not be too long" do
+        @user.name = "a" * 33
+        assert_not @user.valid?
+    end
+
+    test "email should not be too long" do
+        @user.email = "a" * 129
+        assert_not @user.valid?
+    end
+
 end
