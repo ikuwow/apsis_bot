f2ec2d5d5a59af13135c97628e1a8928ff43d59b
Ikuo Degawa <degawa@ikuwow.com>
2015-11-21 18:48:57 +0900

    Add password_digest to uesrs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118144023) do
+ActiveRecord::Schema.define(version: 20151119152900) do
 
   create_table "tweet_logs", force: :cascade do |t|
     t.integer  "tweet_id",   limit: 4, null: false
@@ -43,11 +43,11 @@ ActiveRecord::Schema.define(version: 20151118144023) do
   add_index "twitter_accounts", ["user_id"], name: "fk_rails_287de1faaf", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255, null: false
-    t.string   "email",      limit: 255
-    t.string   "password",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",            limit: 255, null: false
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
