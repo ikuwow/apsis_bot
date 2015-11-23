9f18c8320c310e4ba3fe09153e5ad1b4b8aacfdc
Ikuo Degawa <degawa@ikuwow.com>
2015-11-24 08:30:02 +0900

    Add user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  def show
-      @user = User.find(params[:id])
-      # debugger (stop processing)
-  end
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+        # debugger (stop processing)
+    end
 end
