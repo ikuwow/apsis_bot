c7737528b46b6c9f4191aeb84ccf7169072c4d94
Ikuo Degawa <degawa@ikuwow.com>
2015-11-29 08:09:25 +0900

    Switch view by login status

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,13 @@ module SessionsHelper
         # save encrypted user id in cookie
         session[:user_id] = user.id
     end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+        # last values are returned
+    end
 end
